f16b8f9d6e7f6ce52a101b054b871ab71bb37d05
AlexsJones <jonesax@hush.comm>
2014-06-17 10:23:22 +0100

    threading out listeners

--- src/net/jnxsocket.c
+++ src/net/jnxsocket.c
@@ -28,10 +28,25 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include "jnxthread.h"
 #include "jnxlog.h"
 #include "jnxcheck.h"
 #include "jnxsocket.h"
 #define MAXBUFFER 1024
+
+typedef struct internal_thr_data { 
+	jnx_socket *s;
+	uint8_t *payload;
+	size_t bytesread;
+	int (*callback)(uint8_t *p, size_t br, jnx_socket *s);
+}internal_thr_data;
+
+void* internal_threaded_responder(void *args) {
+	internal_thr_data *thr_data = args;
+	JNXCHECK(thr_data);
+	thr_data->callback(thr_data->payload,thr_data->bytesread,thr_data->s);
+	return 0;
+}
 jnx_socket *create_socket(unsigned int type,unsigned int addrfamily) {
 	JNXCHECK(addrfamily);
 	JNXCHECK(type);
@@ -361,10 +376,12 @@ int jnx_socket_tcp_listen(jnx_socket *s, char* port, ssize_t max_connections, tc
 		fread(out,sizeof(uint8_t),len,fp);
 		fclose(fp);
 
-		int ret = 0;
-		if((ret = c(out,len,s)) != 0) {
-			return 0;
-		}
+		internal_thr_data thr_data;
+		thr_data.payload = out;
+		thr_data.bytesread = len;
+		thr_data.s = s;
+		thr_data.callback = c;
+		jnx_thread_create_disposable(internal_threaded_responder,&thr_data);
 	}
 	return 0;
 }
@@ -424,10 +441,12 @@ int jnx_socket_udp_listen(jnx_socket *s, char* port, ssize_t max_connections, ud
 			fread(out,sizeof(uint8_t),len,fp);
 			fclose(fp);
 
-			int ret = 0;
-			if((ret = c(out,len,s)) != 0) {
-				return 0;
-			}
+			internal_thr_data thr_data;
+			thr_data.payload = out;
+			thr_data.bytesread = len;
+			thr_data.s = s;
+			thr_data.callback = c;
+			jnx_thread_create_disposable(internal_threaded_responder,&thr_data);
 		}
 	}
 	return 0;
