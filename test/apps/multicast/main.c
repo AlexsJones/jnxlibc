1f56ee0f91cdb07e07365be8afb3a9f440ccd4b1
Dragan Glumac <dragan.glumac@gmail.com>
2015-03-21 08:10:51 +0000

    Multicast app

--- test/apps/multicast/main.c
+++ test/apps/multicast/main.c
@@ -18,6 +18,8 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "jnx_udp_socket.h"
 #include "jnxthread.h"
 #include "jnxcheck.h"
@@ -26,8 +28,6 @@
 
 #define TESTPORT "9090"
 #define BGROUP "225.1.1.1"
-
-int test_udp_listener_complete = 0;
 
 /* Function prototypes */
 struct sockaddr *filter_local_ip_address(struct ifaddrs *addr); 
@@ -48,31 +48,34 @@ void test_udp_multicast() {
   JNX_LOG(0,"Using IP => %s",ip);
   jnx_udp_listener *listener = 
     jnx_socket_udp_listener_multicast_create(TESTPORT,AF_INET,ip,BGROUP);
-  
+
   fire_threaded_udp_packet_multicast(TESTPORT);
   while(1) {
     jnx_socket_udp_listener_tick(listener,test_udp_listener_callback,NULL);
-    if(test_udp_listener_complete)break;
   }
   jnx_socket_udp_listener_destroy(&listener);
-  JNXCHECK(test_udp_listener_complete);
   JNXCHECK(listener == NULL);
 }
 void test_udp_listener_callback(const jnx_uint8 *payload,
     jnx_size bytes_read, void *args) {
-  JNX_LOG(0,"test_udp_listener_callback achieved");
-  test_udp_listener_complete = 1;
+  printf("[PACKET] %s\n", payload);
 }
 void fire_threaded_udp_packet_multicast(char *port) {
   jnx_thread_create_disposable(worker_multicast,port);
 }
 void *worker_multicast(void *args) {
-  char *port = (char*)args;
-  jnx_socket *t = jnx_socket_udp_create(AF_INET);
-  char *ip = calloc(16,sizeof(jnx_char));
-  jnx_network_fetch_local_ipv4(ip,filter_local_ip_address);
-  JNX_LOG(0,"Using IP => %s",ip);
-  jnx_socket_udp_multicast_send(t,BGROUP,port,(jnx_uint8 *) "ping",5);
+  while (1) {
+    char *port = (char*)args;
+    jnx_socket *t = jnx_socket_udp_create(AF_INET);
+    char *ip = calloc(16,sizeof(jnx_char));
+    jnx_network_fetch_local_ipv4(ip,filter_local_ip_address);
+    char payload[24];
+    strcpy(payload, "ping < ");
+    strcat(payload, ip);
+    jnx_socket_udp_multicast_send(t,BGROUP,port,
+        (jnx_uint8 *) payload, strlen(payload) + 1);
+    sleep(1);
+  }
   return NULL;
 }
 struct sockaddr *filter_local_ip_address(struct ifaddrs *addr) {
